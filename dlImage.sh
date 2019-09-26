5e42a6bf7b568c9f5ce7ec5ac6fd4eaca024fafd
moonlight83340 <moonlight83340@users.noreply.github.com>
2019-09-26 16:12:59 +0200

    Delete comma and space on image title.

--- dlImage.sh
+++ dlImage.sh
@@ -18,8 +18,15 @@ imageDownload() {
 	name="${3}"
 	images="${image}/${4}"
 	name=$(./parseJson.sh ${1} "${3}")
+	name="${name//\./}"
+	name="${name//\//}"
+	name="${name/ ,/}"  
+	
 	link=$(./parseJson.sh ${1} ${searchName})
-	name=`echo "$name" | sed -e 's/[.*"<>?:|\/]//g'`
+	link="${link// /}"
+	link="${link/,/}"  
+	
+	echo $link
 	
 	if [ "$name" = 'slug":null,' ];then
 		name=$(./parseJson.sh ${1} "name")
@@ -28,8 +35,6 @@ imageDownload() {
 	
 	if [ ! -d "${images}/${name}" ];then
 		mkdir -p "${images}/${name}"
-	#else
-	#	echo "${images}/${name} existe !"
 	fi
 
 	if [ ! -f "${images}/${name}/${name}-original.jpg" ];then
@@ -42,7 +47,7 @@ characterImageDowload() {
 	idStart="${1}"
 	idEnd="${2}"
 	for(( i="${idStart}"; i<="${idEnd}"; i++ ));do
-		if [ ! -f "./characters/${i}" ];then
+		if [ ! -e "./characters/${i}" ];then
 			echo  "./characters/${i} not exist !"
 		else
 			imageDownload "./characters/${i}" "original" "slug" "Characters"
@@ -55,50 +60,31 @@ animeImageDowload() {
 	idStart="${1}"
 	idEnd="${2}"
 	for(( i="${idStart}"; i<="${idEnd}"; i++ ));do
-		if [ ! -f "./animes/${i}/${i}.json" ];then
-			echo  "./animes/${i}/${i}.json not exist !"
-			if [ ! -f "./animes/${i}.json" ];then
-				echo  "./animes/${i}.json not exist !"
-			else
-				imageDownload "./animes/${i}.json" "original" "slug" "Animes"
-			fi
+		if [ ! -e "./anime/${i}" ];then
+			echo  "./anime/${i} not exist !"
 		else
-			imageDownload "./animes/${i}/${i}.json" "original" "slug" "Animes"
+			imageDownload "./anime/${i}" "original" "en_jp" "Animes"
 		fi
-	done
+	done;
 }
 
 mangaImageDownload() {
-	idStart="${1}"
-	idEnd="${2}"
-	for(( i="${idStart}"; i<="${idEnd}"; i++ ));do
-		if [ ! -f "./mangas/${i}/${i}.json" ];then
-			echo  "./mangas/${i}/${i}.json not exist !"
-			if [ ! -f "./mangas/${i}.json" ];then
-				echo  "./mangas/${i}.json not exist !"
-			else
-				imageDownload "./mangas/${i}.json" "original" "slug" "Mangas"
-			fi
-		else
-			imageDownload "./mangas/${i}/${i}.json" "original" "slug" "Mangas"
+	for dir in manga0-40692_chapters0-709205/mangas/*;do
+		if [ -d ${dir} ]; then
+			# Will not run if no directories are available
+			num="${dir##manga0-40692_chapters0-709205/mangas/}"
+			imageDownload "$dir/$num.json" "original" "en_jp" "Mangas"
+		elif [ -f ${dir} ];then
+			imageDownload "$dir" "original" "en_jp" "Mangas"
 		fi
 	done
-	# for dir in manga0-40692_chapters0-709205/mangas/*;do
-	# 	if [ -d ${dir} ]; then
-	# 		# Will not run if no directories are available
-	# 		num="${dir##manga0-40692_chapters0-709205/mangas/}"
-	# 		imageDownload "$dir/$num.json" "original" "en_jp" "Mangas"
-	# 	elif [ -f ${dir} ];then
-	# 		imageDownload "$dir" "original" "en_jp" "Mangas"
-	# 	fi
-	# done
 }
 
 peopleImageDownload() {
 	idStart="${1}"
 	idEnd="${2}"
 	for(( i="${idStart}"; i<="${idEnd}"; i++ ));do
-		if [ ! -f "./people/${i}" ];then
+		if [ ! -e "./people/${i}" ];then
 			echo  "./people/${i} not exist !"
 		else
 			imageDownload "./people/${i}" "image" "name" "Peoples"
@@ -111,10 +97,11 @@ if [ $# -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "-usage" ]
 	usage
 elif [ "${1}" = "-c" ];then
 	characterImageDowload "${2}" "${3}"
+#anime case
 elif [ "${1}" = "-a" ];then
 	animeImageDowload "${2}" "${3}"
 elif [ "${1}" = "-m" ];then
-	mangaImageDownload "${2}" "${3}"
+	mangaImageDownload
 elif [ "${1}" = "-p" ];then
 	peopleImageDownload "${2}" "${3}"
 else
